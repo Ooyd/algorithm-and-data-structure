d8bb28163a21b873126890c2f4ff53cac207a71d
june0929 <92978646+SeongJunBlog@users.noreply.github.com>
2023-08-17 18:13:56 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 136 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -2,40 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-    Queue<Integer> q = new LinkedList<>();
-    Stack<Integer> s = new Stack<>();
-
-    int n = Integer.parseInt(br.readLine());
-    st = new StringTokenizer(br.readLine());
-    for(int i=0; i<n; i++){
-      q.offer(Integer.parseInt(st.nextToken()));
-    }
-    int start = 1;
-
-    while (!q.isEmpty()){ //q가 빌떄까지 반복
-      int frontStudent = q.poll();
-      if(frontStudent == start){
-        start++;
-
-        while (!s.isEmpty() && s.peek() == start){
-          s.pop();
-          start++;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> s = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st =new StringTokenizer(br.readLine());
+        
+        for(int i = 0; i<n; i++){
+            q.offer(Integer.parseInt(st.nextToken()));
         }
-      } else{
-        s.push(frontStudent);
-      }
+        int start = 1;         
+        
+        while(!q.isEmpty()){
+            int watingList = q.poll();
+            if(watingList == start){
+                start++;
+                while(!s.isEmpty() && s.peek() == start){
+                    s.pop();
+                    start++;
+                }
+            } else{
+                s.push(watingList);
+            }
+        }
+                        
+        while(!s.isEmpty() && s.peek() == start){
+            s.pop();
+            start++;
+        }
+                        
+                        
+        System.out.println(s.isEmpty()? "Nice" : "Sad");
     }
-
-    while (!s.isEmpty() && s.peek() == start){ // s이 빌떄까지 반복.
-      s.pop();
-      start++;
-    }
-
-
-    System.out.println((s.isEmpty()? "Nice" : "Sad"));
-
-  }
 }
