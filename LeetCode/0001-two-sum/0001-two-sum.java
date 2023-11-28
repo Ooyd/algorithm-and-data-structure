10a97fe86f4af143208cfc2d09680894ac447584
Ooyd <>
2023-11-28 13:26:57 +0000

    Update from https://github.com/Ooyd/LeetCode/commit/1ac0466ab804d88b6cc988d79665e60d126b692c

---
+++ LeetCode/0001-two-sum/0001-two-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i=0; i < len; i++){
+            if(map.containsKey(target - nums[i])){
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
