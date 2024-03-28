557241a9331a9908367ca55590f909fea0df433b
june0929 <qk1413@naver.com>
2024-03-28 11:35:20 +0900

    [Bronze II] Title: 상수, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        while(sc.hasNextInt()){
+            String A = sc.next();
+            String B = sc.next();
+            int numA = Integer.parseInt(reverseString(A));
+            int numB = Integer.parseInt(reverseString(B));
+        
+            if(numA > numB){
+                System.out.println(numA);
+            } else if (numA < numB){
+                System.out.println(numB);
+            } else{
+                System.out.println(0);
+            }
+        }
+
+        
+        
+        
+    }
+    
+    public static String reverseString(String str){
+        return new StringBuilder(str).reverse().toString();
+    }
+}
