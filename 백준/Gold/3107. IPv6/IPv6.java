d6eb66766b47d84c91620d69c79c5d004faaef46
june0929 <qk1413@naver.com>
2024-04-02 14:32:51 +0900

    [Gold V] Title: IPv6, Time: 120 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Gold/3107. IPv6/IPv6.java
+++ 백준/Gold/3107. IPv6/IPv6.java
@@ -1,66 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputIpv6 = br.readLine();
 
-        String inputIPv6 = br.readLine();
-
-        // IPv6 주소를 :: 기준으로 앞뒤 부분으로 분리
-        String[] ipv6Parts = inputIPv6.split("::");
-
+        String[] parts = inputIpv6.split("::");
         String[] beforeParts;
         String[] afterParts;
 
-        // ::기준 전방향 IPv6 처리
-        if (ipv6Parts[0].isEmpty()) {
-            beforeParts = new String[0];
+        if(parts[0].isEmpty()){
+           beforeParts = new String[0];
         } else {
-            beforeParts = ipv6Parts[0].split(":");
+            beforeParts = parts[0].split(":");
         }
 
-        // ::기준 후방향 IPV6 처리
-        if (ipv6Parts.length == 2) {
-            afterParts = ipv6Parts[1].split(":");
-        } else {
+        if(parts.length == 2){
+            afterParts = parts[1].split(":");
+        } else{
             afterParts = new String[0];
         }
 
-        //전방향, 후방향 제외한 0000이 들어갈 그룹갯수
-        int missingGroups = 8 - (beforeParts.length + afterParts.length);
+        int emptyValueCount = 8 -(beforeParts.length + afterParts.length);
 
-        StringBuilder fullIPv6 = new StringBuilder();
+        StringBuilder fullIpv6 = new StringBuilder();
 
-        for (String part : beforeParts) {
-            StringBuilder paddedPart = new StringBuilder(part);
-            while (paddedPart.length() < 4) {
-                paddedPart.insert(0,"0");
+        for(String part : beforeParts){
+            StringBuilder tempPart = new StringBuilder(part);
+
+            while(tempPart.length() < 4){
+                tempPart.insert(0,"0");
             }
-            fullIPv6.append(paddedPart).append(":");        }
-
-        for (int i = 0; i < missingGroups; i++) {
-            fullIPv6.append("0000:");
+            fullIpv6.append(tempPart).append(":");
         }
 
-        for (int i = 0; i < afterParts.length; i++) {
-            StringBuilder paddedPart = new StringBuilder(afterParts[i]);
-            while (paddedPart.length() < 4) {
-                paddedPart.insert(0, '0');
-            }
-            fullIPv6.append(paddedPart);
-            if (i < afterParts.length - 1) {
-                fullIPv6.append(":");
-            }
+        for(int i=0; i<emptyValueCount;i++){
+            fullIpv6.append("0000:");
         }
 
-        if (fullIPv6.charAt(fullIPv6.length() - 1) == ':') {
-            fullIPv6.deleteCharAt(fullIPv6.length() - 1);
+        for(String part : afterParts){
+            StringBuilder tempPart = new StringBuilder(part);
+            while(tempPart.length() < 4){
+                tempPart.insert(0,"0");
+            }
+            fullIpv6.append(tempPart).append(":");
         }
 
+        if(fullIpv6.charAt(fullIpv6.length()-1) == ':'){
+            fullIpv6.deleteCharAt(fullIpv6.length()-1);
+        }
 
-        System.out.println(fullIPv6.toString());
+        System.out.println(fullIpv6.toString());
+
     }
 }
