365f18d99a363443851731693f508d8dcdf39924
june0929 <qk1413@naver.com>
2024-04-03 14:31:25 +0900

    [Silver III] Title: 프린터 큐, Time: 156 ms, Memory: 14916 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -12,13 +12,13 @@ public class Main{
      */
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스의 수
         ArrayList<Integer> res = new ArrayList<>();
 
         for(int i =0; i<T; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken()); // 문서 개수
+            int M = Integer.parseInt(st.nextToken()); // 궁금한 문서의 위치
             
             st = new StringTokenizer(br.readLine());
             int temp = solve(st,N,M);
@@ -27,7 +27,7 @@ public class Main{
 
         }
 
-
+        // 각 테스트 케이스의 결과 출력
         for(int num : res){
             System.out.println(num);
         }
@@ -37,36 +37,36 @@ public class Main{
 
     private static int solve(StringTokenizer st, int N, int M) {
         Queue<Integer> queue = new LinkedList<>();
-        int[] documents = new int[N];
-        int printOrder = 0;
+        int[] documents = new int[N]; // 문서의 중요도 저장
+        int printOrder = 0; // 인쇄 순서
 
-        for(int i =0; i<N; i++){
-            documents[i] = Integer.parseInt(st.nextToken());
-            queue.offer(i);
+        for(int i = 0; i < N; i++){
+            documents[i] = Integer.parseInt(st.nextToken()); // 중요도 저장
+            queue.offer(i); // 문서의 위치 저장
         }
 
         while(!queue.isEmpty()){
-            int current = queue.poll();
-            boolean isMax = true;
+            int current = queue.poll(); //현재 문서 확인
+            boolean isMax = true; //가장 중요한 문서라고 가정
 
+            //큐에 남아있는 모든 문서를 확인하며 현재 문서보다 중요한게 있는지 체크.
             for(int i : queue) {
                 if (documents[current] < documents[i]) {
                     isMax = false;
+                    queue.offer(current);
                     break;
                 }
             }
 
+            //현재 문서가 중요한 문서면은 인쇄 처리
             if(isMax){
-                printOrder++;
-                if (current == M){
-                    return printOrder;
+                printOrder++; //순서 증가
+                if (current == M){ 
+                    return printOrder; //인쇄순서 반환.
                 }
-            } else {
-                queue.offer(current);
             }
-
         }
 
-        return printOrder;
+        return 0;
     }
 }
