40a9ba66e2b344a963b365f7d1b3e1e3278f9c99
june0929 <qk1413@naver.com>
2024-04-05 13:22:22 +0900

    Update ATM.java

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -7,7 +7,15 @@ import java.io.*;
  * 2. 개념 및 접근법
  * - 오름차순으로 정렬하여 최소값부터 계산
  * 3. 풀이 결과
- * 메모리  ,시간
+ * 메모리 14536 ,시간 144
+ * 4. 추가 정리
+ * - 처음에는 우선순위 큐를 사용한다 생각했는데, Array sort를 써도 된다라고 생각이들고, 같은 기능인데 어떤 상황일떄 쓰는게 유리할지 정리해보았다.
+ * - Array 장점 :
+ *      - 전체 데이터를 한 번에 메모리에 로딩, `Arrays.sort()`같은 효율적인 정렬 알고리즘을 사용해 빠르게 정렬가능.(Tim sort알고리즘)
+ *      - 즉 데이터가 정적이고, 한번에 정렬을 해야 한다면 Array가 유리하다.
+ * - Priority Queue :
+ *      - 삽입 및 삭제 연산이 자주 발생하는 동적인 데이터셋에 유용, 왜냐하면 각 삽입 및 삭제, 즉 데이터 변동이 있을때마다 내부적으로 정렬을 유지하기에 자원을 더 잡아먹음.
+ *      - 즉 데이터가 동적이고, 삽입 및 삭제가 빈번하게 발생하는 상황일떄 Priority Queue가 유리하다.
  */
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -24,7 +32,7 @@ public class Main {
         }
 
         int total = 0; //총 대기 시간
-        int sum = 0; // 각 사람별 대기 시간 누적 합
+        int sum = 0; // 현재 사람들의 대기시간 합
         //조건: 사람들이 전부 인출할때까지
         while(!pq.isEmpty()){
             sum += pq.poll();
