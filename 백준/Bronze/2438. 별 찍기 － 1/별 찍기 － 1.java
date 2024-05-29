813649fd8e30ed6ebf7327b4e4f702cd6248157d
june0929 <qk1413@naver.com>
2024-05-29 09:21:37 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,12 +1,13 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         
         for(int i = 1; i <= N; i++){
-            String stars = new String(new char[i]).replace("\0", "*");
+            String stars = new String(new char[i]).replace("\0","*");
             System.out.println(stars);
         }
     }
